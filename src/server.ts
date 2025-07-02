feb0b0d25030764f16de005eef9adedf3d9fea7e
Israel Teixeira <israeltx@gmail.com>
2025-07-02 12:31:33 -0300

    Create a POST request

--- src/server.ts
+++ src/server.ts
@@ -3,10 +3,16 @@ import express from 'express'
 const PORT = 3333
 
 const app = express()
+app.use(express.json())
 
 app.get('/products', (request, response) => {
   const {page, limit} = request.query
   response.send(`${page} ${limit}`)
+})
+
+app.post('/products', (request, response) => {
+  const {name, price} = request.body
+  response.send(`${name} ${price}`)
 })
 
 app.listen(PORT, () => console.log('server is on'))
