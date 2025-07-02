996cba56d29b3505855f16cd42bb7e79fa566a31
Israel Teixeira <israeltx@gmail.com>
2025-07-02 11:46:56 -0300

    Teste typescript compilation

--- src/server.js
+++ src/server.js
@@ -1,1 +1,5 @@
-console.log("Hello World");
+function sum(a, b) {
+    return a + b;
+}
+var result = sum(2, 2);
+console.log(result);
---
+++ src/server.ts
@@ -0,0 +1,6 @@
+function sum(a:number, b:number):number {
+  return a + b
+}
+
+var result = sum(2,2);
+console.log(result);
