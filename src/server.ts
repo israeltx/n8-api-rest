5ac5b7bbfdae4cf06b59cebfdc72809ab30ba89f
Israel Teixeira <israeltx@gmail.com>
2025-07-02 12:23:31 -0300

    Set query params for the GET request

--- src/server.ts
+++ src/server.ts
@@ -4,9 +4,9 @@ const PORT = 3333
 
 const app = express()
 
-app.get('/products/:id', (request, response) => {
-  const {id} = request.params
-  response.send(id)
+app.get('/products', (request, response) => {
+  const {page, limit} = request.query
+  response.send(`${page} ${limit}`)
 })
 
 app.listen(PORT, () => console.log('server is on'))
