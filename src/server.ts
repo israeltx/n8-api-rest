3f73b953bc5ff18fa42cda4992407531c5e1e3a8
Israel Teixeira <israeltx@gmail.com>
2025-07-02 16:41:10 -0300

    Format error messages

--- src/server.ts
+++ src/server.ts
@@ -2,6 +2,7 @@ import express from 'express'
 import { routes } from './routes'
 import { Request, Response, NextFunction } from "express";
 import { AppError } from './utils/AppError';
+import { ZodError } from 'zod';
 
 const PORT = 3333
 
@@ -11,9 +12,18 @@ app.use(routes)
 // Exceptions treatment
 app.use((error:any, request:Request, response:Response, _:NextFunction) => {
 
-  // if (error instanceof AppError) {
-  //   return response.status(error.statusCode).json({message: error.message})
-  // }
+  if (error instanceof AppError) {
+    return response.status(error.statusCode).json({message: error.message})
+  }
+
+  if (error instanceof ZodError) {
+    return response.status(400).json(
+      {
+        message: 'Validation error',
+        issues: error.format()
+      }
+    )
+  }
 
   response.status(500).json(
     { message: error.message }
