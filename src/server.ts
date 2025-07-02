3ad635d9bdbbbaedabcde15d7bb3ab97f8ad5e8b
Israel Teixeira <israeltx@gmail.com>
2025-07-02 12:19:50 -0300

    Set route params for the GET request

--- src/server.ts
+++ src/server.ts
@@ -4,8 +4,9 @@ const PORT = 3333
 
 const app = express()
 
-app.get('/', (request, response) => {
-  response.send('Olá')
+app.get('/products/:id', (request, response) => {
+  const {id} = request.params
+  response.send(id)
 })
 
 app.listen(PORT, () => console.log('server is on'))
